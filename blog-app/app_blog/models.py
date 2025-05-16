ec26e981af511465383ce28a41d486fe61cdf867
mast3r <mast3r1mba@gmail.com>
2025-05-16 11:23:42 +0500

    Add files via upload

--- blog-app/app_blog/models.py
+++ blog-app/app_blog/models.py
@@ -1,3 +1,59 @@
+from datetime import datetime
+
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Categories(models.Model):
+    name = models.CharField(max_length=20)
+    description = models.TextField()
+    icon = models.TextField(null=True)
+
+
+class Articles(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    preview = models.CharField(max_length=50, null=True)
+    date_create = models.DateTimeField(auto_now_add=True, blank=True)
+    date_update = models.DateTimeField(null=True)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+    category = models.ForeignKey(
+        Categories,
+        on_delete=models.SET_NULL,
+        null=True
+    )
+
+
+class Comments(models.Model):
+    content = models.TextField()
+    date_create = models.DateTimeField(auto_now_add=True, blank=True)
+    date_update = models.DateTimeField(null=True)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+    article = models.ForeignKey(
+        Articles,
+        on_delete=models.CASCADE
+    )
+
+
+class Reactions(models.Model):
+    is_like = models.BooleanField()
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+    article = models.ForeignKey(
+        Articles,
+        on_delete=models.CASCADE,
+        null=True
+    )
+    comment = models.ForeignKey(
+        Comments,
+        on_delete=models.CASCADE,
+        null=True
+    )
--- blog-app/app_blog/views.py
+++ blog-app/app_blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def home_page(request):
+    return render(
+        request,
+        "app_blog/home.html",
+        {"title": "Главная страница"}
+    )
